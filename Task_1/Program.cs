53fd5a1fcb172975cc6e1a828841c1e86422365e
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-24 21:39:37 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = LengthOfCircle;
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Длинна окружности: {myDelegate(r)} ");
+            myDelegate = SquareOfCircle;
+            Console.WriteLine($"Площадь круга: {myDelegate(r)} ");
+            myDelegate = VolumeOfSphere;
+            Console.WriteLine($"Объем шара: {myDelegate(r)} ");
+            Console.ReadKey();
+        }
+
+        static double LengthOfCircle(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static double SquareOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        static double VolumeOfSphere(double r)
+        {
+            return 4 / 3 * Math.PI * Math.Pow(r, 3);
+        }
+
+    }
+}
